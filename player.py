8704dce1640ce14e0886ffadaf094bf01a9dd5b9
R1baszzZ <mr.guiribas@gmail.com>
2026-02-12 13:14:50 -0500

    Shooting speed fixed, caused a delay of 0.3 seconds

--- player.py
+++ player.py
@@ -8,6 +8,8 @@ class Player(CircleShape):
         self.x = x
         self.y = y
         self.rotation = 0
+        self.shot_cooldown = 0
+
 
 
     def triangle(self):
@@ -33,6 +35,7 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.shot_cooldown -= dt
 
         if keys[pygame.K_a]:
             self.rotate(dt*-1)
@@ -46,6 +49,10 @@ class Player(CircleShape):
             self.shoot()
 
     def shoot(self):
-        shot = Shot(self.position.x, self.position.y)
-        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        if self.shot_cooldown > 0:
+            return
+        else:
+            self.shot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
+            shot = Shot(self.position.x, self.position.y)
+            shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
          
