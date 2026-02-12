bf81e88686dfd0836ec89d9b75db0dcd5197be0a
R1baszzZ <mr.guiribas@gmail.com>
2026-02-12 00:57:22 -0500

    Fixed closing button. it was comaparing the object itself, not the .type

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
-            if event == pygame.QUIT:
+            if event.type == pygame.QUIT:
                 return
-                
+        dt = clock.tick(60) / 1000
 
         screen.fill("black")
         pygame.display.flip()
